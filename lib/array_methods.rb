5850262f80fc22b2a89707b2bf8a7bc04f5f9e71
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 10:19:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,11 @@ end
 def find_max_value(array)
   curr_max = array[0]
   array.length.times do |index|
-    
+    if array[index] > curr_max
+    curr_max = array[index]
+    end
+  end
+  return curr_max
 end
 
 def find_min_value(array)
