362d910afc3fda9901cf6fa48b1e4fc437736d08
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 10:24:53 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,28 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |index|
+    if array[index] === value_to_find
+      return index
+    end
+  end
+  return nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  curr_max = array[0]
+  array.length.times do |index|
+    if array[index] > curr_max
+    curr_max = array[index]
+    end
+  end
+  return curr_max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  curr_min = array[0]
+  array.length.times do |index|
+    if array[index] < curr_min
+      curr_min = array[index]
+    end
+  end
+  return curr_min
 end
